dd6b44a0fa412960f26fc6ffb856a3c442eace6f
Nikola92-prog <61543550+Nikola92-prog@users.noreply.github.com>
2020-12-20 11:48:10 +0100

    Update app.js

--- 04. BudgetApp/app.js
+++ 04. BudgetApp/app.js
@@ -89,6 +89,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 */
 
+
+/*
 //
 // READING INPUT DATA
 
@@ -161,3 +163,90 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
 })(budgetController, UIController);
+
+*/
+
+//
+// CREATING AN INITIALIZATION FUNCTION 
+
+
+// BUDGET CONTROLLER
+var budgetController = (function(){
+
+    //SOME CODE
+
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+            
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+
+    };
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function() {
+        
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+
+            if(event.keyCode === 13) {
+                ctrlAddItem();
+            }
+
+        });
+    };
+
+    var ctrlAddItem = function() {
+
+        // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        //console.log('It works!');
+    }
+
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    }
+
+})(budgetController, UIController);
+
+
+controller.init();
