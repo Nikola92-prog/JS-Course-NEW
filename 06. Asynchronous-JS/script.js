9c9c096917ca250af03b30ed7dbf9a20241eee4d
Nikola92-prog <61543550+Nikola92-prog@users.noreply.github.com>
2021-01-19 17:11:34 +0100

    Update script.js

--- 06. Asynchronous-JS/script.js
+++ 06. Asynchronous-JS/script.js
@@ -139,7 +139,21 @@ getRecipesAW();
 //___Making AJAX Calls with Fetch & Promises___//
 /////////////////////////////////////////////////
 
-
+function getWeather(woeid) {
+    fetch
+    (`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
+    .then(result => {
+        console.log(result);
+        return result.json();
+    })
+    .then(data => {
+        const today = data.consolidated_weather[0];
+        console.log(`Temperatures in ${data.title} stay between ${today.min_temp} and ${today.max_temp}.`);
+    })
+    .catch(error => console.log(error));
+}
+getWeather(2487956);
+getWeather(44418);
 
 
 
